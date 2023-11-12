85d6a412d166845c7a1a42b5b39cbba9d5e1c96b
msubburao <msubbu.rao11@gmail.com>
2023-11-11 22:55:29 -0800

    redischeck

---
+++ src/main/java/com/subbums/redischeck/RedischeckApplication.java
@@ -0,0 +1,13 @@
+package com.subbums.redischeck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedischeckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedischeckApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/subbums/redischeck/RedischeckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subbums.redischeck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedischeckApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
